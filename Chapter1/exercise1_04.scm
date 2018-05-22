17ece76de9c956c8b1163dad9e0fc0d3254a71ba
Ademola Oyewale <saopayne@gmail.com>
2018-05-22 06:08:17 +0100

    Add exercise 1.4 and 1.5 solutions

---
+++ Chapter1/exercise1_04.scm
@@ -0,0 +1,14 @@
+;; Exercise 1.4.  Observe that our model of evaluation allows for
+;; combinations whose operators are compound expressions. Use this
+;; observation to describe the behavior of the following procedure:
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+;; ANSWER ------------------------------------------------------------
+
+;; (a-plus-abs-b 2 3)
+;; ((if (> 3 0) + -) 2 3)    ;; First expansion
+;; ((if true + -) 2 3)       ;; Expand condition
+;; (+ 2 3)                   ;; Substitute if
+;; 5                         ;; Substitute the addition
